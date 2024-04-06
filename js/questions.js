2ac403a73a1983baf34750b0f914a5e696085168
vidhushii <vidhushi8434@gmail.com>
2024-04-06 12:39:44 +0530

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options and answers
+let questions=[
+    {
+        numb: 1,
+        question: "SMTP in networking meant for?",
+        answer: "Simple Mail Transfer Proto­col",
+        options:[
+            "Simple Mail Transfer Proto­col",
+            "Simple Message Transfer Protocol",
+            "Simple Mail Transport protocol",
+            "System Mail Transport Protocol"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the full form of GPT in Chat GPT?",
+        answer: "Generative Pre-Trained Transformer",
+        options:[
+            "GUID Partition Table",
+            "Grooved Pegboard Test",
+            "Generative Pre-Trained Transformer",
+            "Glutamic Pyruvic Trans­aminase"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Related to computers, what is meant by software?",
+        answer: "computer programs",
+        options:[
+            "floppy discs",
+            "computer programs",
+            "computer circuitry",
+            "human brain"
+        ]
+    },
+    {
+        numb: 4,
+        question: "_____________________ is unwanted e-mail or junk mail.",
+        answer: "Spam",
+        options:[
+            "+Bomb",
+            "phatBot",
+            "Spam",
+            "Worm"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In the context of the Internet, what is the full form of a URL?",
+        answer: "Uniform Resource Locator",
+        options:[
+            " User Requested Link",
+            "Ultimate Response Location",
+            "Unique Request Locator",
+            "Uniform Resource Locator"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is the full form of RAM?",
+        answer: "Random Access Memory",
+        options:[
+            "Random Access Memory",
+            "Read And Memory",
+            "Random All Memory",
+            "None of the above mentioned"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Who is the founder of Facebook?",
+        answer: "Mark Zuckerberg",
+        options:[
+            "Andrew Maclin",
+            "Mark Zuckerberg",
+            "Mark Adon",
+            "Mark mensionier"
+        ]
+    },
+    {
+        numb: 8,
+        question: "HTML stands for_______.",
+        answer: "Hyper Text Markup Language",
+        options:[
+            "Hyperactive Text Markup Language",
+            "Hyper Text Markup Language",
+            "Hyper Text Machine Language",
+            "Higher Text Markup Language"
+        ]
+    },
+    {
+        numb: 9,
+        question: "The correct sequence of HTML tags for starting a webpage is -",
+        answer: "HTML, Head, Title, Body",
+        options:[
+            "Head, Title, HTML, body",
+            "HTML, Body, Title, Head",
+            "HTML, Head, Title, Body",
+            "HTML, Head, Title, Body"
+        ]
+    },
+    {
+        numb: 10,
+        question: "The <hr> tag in HTML is used for -",
+        answer: "horizontal ruler",
+        options:[
+            "horizontal ruler",
+            "new paragraph",
+            "vertical ruler",
+            "new line"
+        ]
+    },
+
+];
